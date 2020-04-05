c3a444b3aa73786d28c0cb226a1b0dc259271862
e2dubba <svirgilgoodwin@gmail.com>
2020-04-05 07:44:45 -0500

    adding docstrings

--- scripts/name_database_generator.py
+++ scripts/name_database_generator.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""
+This script creates, and fills the relational database used for other parts of the code.
+It relies on the yaml file as well as a csv exported from OpenRefine
+"""
 
 import sqlite3
-import yaml
 import csv
+import yaml
 
 
 CONN = sqlite3.connect("amarna_name.db")
@@ -64,6 +68,46 @@ def table_creator():
     CONN.commit()
 
 
+def sql_importfunctions(name_dict, num):
+    """Runs the import queries for each of the items int he name_dict"""
+    for name, item_dict in name_dict.items():
+        print(name)
+        # Columns: canonical_name, scope_note, language, city_id, associated_city
+        CUR.execute(
+            """
+        INSERT INTO canonical_names (canonical_name)
+        VALUES (?)""",
+            (name,),
+        )
+        name_id = CUR.lastrowid
+
+        item_processor = {
+            "LA": {"table_name": "canonical_names", "column": "language",},
+            "SN": {"table_name": "canonical_names", "column": "scope_note",},
+        }
+        query = '''UPDATE {}
+        SET {} = "{}"
+        WHERE name_id = {}
+        AND canonical_name = "{}"'''
+
+        for key, items in item_dict.items():
+            if key == "UF":
+                for item in items:
+                    var_query = "INSERT INTO name_variants (name_id, name_variant) VALUES (?, ?)"
+                    CUR.execute(var_query, (name_id, item))
+            else:
+                try:
+                    table_name = item_processor[key]["table_name"]
+                    column = item_processor[key]["column"]
+                    CUR.execute(query.format(table_name, column, items, name_id, name))
+                except KeyError:
+                    pass
+        num += 1
+        if num % 100 == 0:
+            CONN.commit()
+        return num
+
+
 def import_canonical_names(yml_file):
     """import the canonical names from a Yml file"""
     with open(yml_file) as ymfp:
@@ -73,43 +117,7 @@ def import_canonical_names(yml_file):
     num = 0
     for name_dict in names:
         if isinstance(name_dict, dict):
-            for name, item_dict in name_dict.items():
-                print(name)
-                # Columns: canonical_name, scope_note, language, city_id, associated_city
-                CUR.execute(
-                    """
-                INSERT INTO canonical_names (canonical_name)
-                VALUES (?)""",
-                    (name,),
-                )
-                name_id = CUR.lastrowid
-
-                item_processor = {
-                    "LA": {"table_name": "canonical_names", "column": "language",},
-                    "SN": {"table_name": "canonical_names", "column": "scope_note",},
-                }
-                query = '''UPDATE {}
-                SET {} = "{}"
-                WHERE name_id = {}
-                AND canonical_name = "{}"'''
-
-                for key, items in item_dict.items():
-                    if key == "UF":
-                        for item in items:
-                            var_query = "INSERT INTO name_variants (name_id, name_variant) VALUES (?, ?)"
-                            CUR.execute(var_query, (name_id, item))
-                    else:
-                        try:
-                            table_name = item_processor[key]["table_name"]
-                            column = item_processor[key]["column"]
-                            CUR.execute(
-                                query.format(table_name, column, items, name_id, name)
-                            )
-                        except KeyError:
-                            pass
-                num += 1
-                if num % 100 == 0:
-                    CONN.commit()
+            num = sql_importfunctions(name_dict, num)
     CONN.commit()
 
 
@@ -117,12 +125,12 @@ def import_names_and_context(csv_file):
     """
     reference TEXT,
     line TEXT,
-    extracted_name TEXT, 
+    extracted_name TEXT,
     normalized_name TEXT,
     cluster_key_collision TEXT,
-    cluster_levenshtein TEXT, 
+    cluster_levenshtein TEXT,
     cluster_levenshtein_radius2 TEXT,
-    cluster_levenshtein_block5 TEXT 
+    cluster_levenshtein_block5 TEXT
     """
     query = """INSERT INTO names_and_context (reference, line, extracted_name,
     normalized_name, cluster_key_collision, cluster_levenshtein,
@@ -130,7 +138,7 @@ def import_names_and_context(csv_file):
     VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
     with open(csv_file) as csvfile:
         name_reader = csv.reader(csvfile, delimiter=",")
-        headers = next(name_reader)
+        _ = next(name_reader)  # headers
         num = 0
         for row in name_reader:
             CUR.execute(query, tuple(row[1:]))
@@ -141,6 +149,7 @@ def import_names_and_context(csv_file):
 
 
 def related_term_insert(name_id, related_names):
+    """Insert each of the related terms on its own line"""
     query = "SELECT name_id FROM canonical_names WHERE canonical_name = ?"
     if isinstance(related_names, list):
         for related in related_names:
@@ -170,6 +179,8 @@ def related_term_insert(name_id, related_names):
 
 
 def import_related_and_use(wpacv):
+    """Parse the Yaml file for both related and use terms, if they exist,
+    populate the database with them"""
     with open(wpacv) as wpfp:
         a_dict = yaml.safe_load(wpfp)
     for name_dict in a_dict["Names"]:
@@ -205,6 +216,7 @@ def import_related_and_use(wpacv):
 
 
 def main():
+    """Main function, handles command line arguments"""
     arg_parser = argparse.ArgumentParser(
         description="A script to handle the creation of tables"
     )
@@ -219,7 +231,8 @@ def main():
     arg_parser.add_argument(
         "-u",
         "--use",
-        help="specify a WPACV yaml file to fill in use and related terms. Must be run after the database is built",
+        help="specify a WPACV yaml file to fill in use and related terms."
+        + "Must be run after the database is built",
     )
 
     args = arg_parser.parse_args()
